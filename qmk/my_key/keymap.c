90eed62ff7f0bff77cbdfac40f3585ed744bed4f
hiroki <wahaha826@gmail.com>
2018-03-18 18:57:40 +0900

    new

--- qmk/my_key/keymap.c
+++ qmk/my_key/keymap.c
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QW] = KEYMAP(
   JP_ZHTG, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    JP_CIRC, \
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JP_BSLS, \
-  KC_LCTL, KC_LGUI, KC_LALT, MO(_VIM), TO(_LW),  KC_SPC,  KC_ENT, TO(_RS), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT \
+  KC_LCTL, KC_LGUI, KC_LALT, MO(_VIM), TO(_LW),  KC_SPC,  KC_BSPC, TO(_RS), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT \
  ),
 
 /* Raise
@@ -68,9 +68,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RS] = KEYMAP( \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   JP_ZHTG, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   TO(_LW), KC_4,    KC_5,    KC_6,    KC_PPLS, KC_PMNS, \
-  _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TAB,  KC_1,    KC_2,    KC_3,    KC_PAST, KC_PSLS, \
-  _______, _______, KC_F1,   KC_F12,  _______, TO(_QW), KC_ENT, KC_PEQL,  KC_0,    KC_DOT,  KC_NLCK, _______  \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   TO(_LW), KC_4,    KC_5,    KC_6,    _______, KC_ENT, \
+  _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TAB,  KC_1,    KC_2,    KC_3,    _______, _______, \
+  _______, _______, KC_F1,   KC_F12,  _______, TO(_QW), KC_BSPC, KC_PEQL,  KC_0,    KC_DOT,  _______, _______  \
 ),
 
 /* Lower
@@ -88,9 +88,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LW] = KEYMAP( \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  JP_ZHTG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSPC, \
-  _______, _______, _______, _______, _______, _______, TO(_RS), JP_AT,   _______, _______, JP_AT  , _______, \
-  KC_LSFT, _______, _______, _______, _______, _______, _______, JP_CIRC, _______, _______, JP_CIRC, _______, \
+  JP_ZHTG, _______, _______, _______, _______, _______, _______, _______, JP_LPRN, JP_RPRN, _______, KC_BSPC, \
+  _______, _______, _______, _______, _______, _______, TO(_RS), JP_AT,   JP_LCBR, JP_RCBR, JP_AT  , _______, \
+  KC_LSFT, _______, _______, _______, _______, _______, _______, JP_CIRC, JP_LBRC, JP_RBRC, JP_CIRC, _______, \
   KC_LCTL, _______, KC_LALT, _______, _______, TO(_QW), KC_ENT,  _______, _______, _______, _______, _______  \
 ),
 
